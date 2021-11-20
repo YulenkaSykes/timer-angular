b8b1a290b05e110c015e37245a87a88c2add102c
YulenkaSykes <72409591+YulenkaSykes@users.noreply.github.com>
2021-11-20 16:46:26 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {timer, interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timer-angular';
+
+  rxInterval = interval(1000);
+  rxIntervalSubcription: any;
+
+  time =  new Date(0);
+  goTime = false;
+  clicked = false;
+
+  timeFixer = (str: number) => {
+    return str.toString().length < 2 ? "0" + str : str;
+  }
+
+  stopTimer = () =>{
+    this.rxIntervalSubcription.unsubscribe();
+    this.goTime = false;
+  }
+  
+  startTimer = () =>{
+    this.rxIntervalSubcription = this.rxInterval.subscribe((n) =>
+      this.time = new Date(this.time.setUTCSeconds(this.time.getUTCSeconds() + 1))
+    );
+    this.goTime = true;
+  }
+
+  handleTimerStart = () => {
+    if (!this.goTime) {
+      this.startTimer();
+    } else {
+      this.stopTimer();
+      this.time.setUTCSeconds(0);
+    }
+  };
+
+
+  handleWait = () => {
+    if (this.clicked) {
+      this.stopTimer();
+    } else {
+      this.clicked = true;
+      timer(500).subscribe((time) => this.clicked = true);
+    }
+  }
+
+  handleReset = () => {
+      this.stopTimer();
+      this.time.setUTCSeconds(0);
+      this.startTimer();
+  }
 }
